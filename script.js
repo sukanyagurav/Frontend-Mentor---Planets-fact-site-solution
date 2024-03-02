7c43272f0b2fedf4b4bba43e30022073c599730d
sukanya gurav <sukanyagurav6@gmail.com>
2024-03-02 12:11:45 +0530

    added javascript

--- script.js
+++ script.js
@@ -34,8 +34,9 @@ fetchData()
 
 
 function renderData(planet){
+    document.body.removeAttribute('class')
     const color = getComputedStyle(document.documentElement).getPropertyValue(`--${planet.name.toLowerCase()}`)
-
+    document.body.classList.add(planet.name.toLowerCase())
     const templateEle= document.importNode(planetTemplate.content,true) 
     const planetName = templateEle.querySelector('.planet__name')
     planetName.innerHTML = planet.name
@@ -59,88 +60,45 @@ function renderData(planet){
 
     const radiusValue = templateEle.querySelector('.radiusValue')
     radiusValue.innerHTML = planet.radius
-    console.log(planet)
     const averageValue = templateEle.querySelector('.averageValue')
     averageValue.innerHTML = planet.temperature
     const tabs = templateEle.querySelector('.planet__tabs')
 
-    // if (window.matchMedia("(min-width: 800px)").matches){
-    //     const allTabs = tabs.querySelectorAll('button')
-    //     allTabs.forEach(tab=>{
-    //         tab.addEventListener('mouseenter',function(e){
-    //            tab.classList.add('hover')
-    //         })
-    //         if(!tab.classList.contains('active')){
-    //             tab.addEventListener('mouseleave',function(e){
-    //                 tab.classList.remove('hover')
-    //             })
-    //         }
-    //     })
-
-    // }
-    // allTabs.forEach(tab=>{
-    //     tab.addEventListener('mouseenter',function(e){
-    //         tab.classList.add('hover')
-    //     })
-    //     if(!tab.classList.contains('active')){
-    //         tab.addEventListener('mouseleave',function(e){
-    //             tab.classList.remove('hover')
-    //         })
-    //     }
-    // })
-    tabs.addEventListener('click',function(e){
-        const allTabs = tabs.querySelectorAll('button')
-        allTabs.forEach(tab=>{
-            if(tab.classList.contains('active')){
-                tab.style.backgroundColor=''
-                tab.classList.remove('active')
-            }
-            if(tab.classList.contains('hover')){
-        
-                tab.classList.remove('hover')
-            }
-            // if(!tab.classList.contains('active')){
-            //     tab.addEventListener('mouseleave',function(e){
-            //         tab.style.backgroundColor='transparent'
-            //     })
-            //     tab.addEventListener('mouseenter',function(e){
-            //         tab.style.backgroundColor=color
-            //     })
-            // }
-            // else{
-            //     console.log('contains')
-            //     tab.addEventListener('mouseleave',function(e){
-            //         tab.style.backgroundColor=color
-            //     })
-            // }
-        })
-       
-       if(e.target.closest('#overview')){
-        console.log(  document.querySelector('#overview').firstElementChild)
+    const overview = templateEle.querySelector('#overview')
+    const allTabs = tabs.querySelectorAll('button')
+    overview.addEventListener('click',function(){
+            removeAllActiveClassess(allTabs)
             document.querySelector('#overview').firstElementChild.classList.add('active')
             document.querySelector('.active').style.backgroundColor= color
             internalTab(planetImage,planetDescription,planetSourceLink,planet.overview,planet.images.planet)
-       }
-       else if(e.target.closest('#structure')){
+    })
+    const structure = templateEle.querySelector('#structure')
+    structure.addEventListener('click',function(){
+        removeAllActiveClassess(allTabs)
         document.querySelector('#structure').firstElementChild.classList.add('active')
         document.querySelector('.active').style.backgroundColor= color
         internalTab(planetImage,planetDescription,planetSourceLink,planet.structure,planet.images.internal)
-       }
-
-       else if(e.target.closest('#geology')){
-        document.querySelector('#geology').firstElementChild.classList.add('active')
-        document.querySelector('.active').style.backgroundColor= color
-        internalTab(planetImage,planetDescription,planetSourceLink,planet.geology,planet.images.geology)
-       }
-       else{
-        return
-       }
-
+    })
+    const geology = templateEle.querySelector('#geology')
+    geology.addEventListener('click',function(){
+            removeAllActiveClassess(allTabs)
+            document.querySelector('#geology').firstElementChild.classList.add('active')
+            document.querySelector('.active').style.backgroundColor= color
+            internalTab(planetImage,planetDescription,planetSourceLink,planet.geology,planet.images.geology)
+    
     })
     main.appendChild(templateEle)
     
 
-}   
+} 
+function removeAllActiveClassess(allTabs){
+    allTabs.forEach(tab=>{
+        if(tab.classList.contains('active')){
+            tab.style.backgroundColor=''
+            tab.classList.remove('active')
+        }
+    })
+}  
 function internalTab(planetImage,planetDescription,planetSourceLink,data,image){
     planetImage.src=image
     planetDescription.innerHTML = data.content
@@ -148,7 +106,6 @@ function internalTab(planetImage,planetDescription,planetSourceLink,data,image){
 }
 nav__link.addEventListener('click',function(e){
     const links = nav__link.querySelectorAll('button')
-    console.log(links)
         links.forEach(link=>{
             if(link.classList.contains('planet__active')){
                 link.classList.remove('planet__active')
